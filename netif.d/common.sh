91a721da68c33baed6a69e3ff981ec707c5bb3ee
Daniel Robbins <drobbins@funtoo.org>
2010-07-20 13:36:01 -0600

    warning if no netmask in new format is defined

--- netif.d/common.sh
+++ netif.d/common.sh
@@ -11,6 +11,22 @@ mtu=${mtu:-$MTU}
 nameservers="${nameservers:-$NS1 $NS2}"
 gateway=${gateway:-$GW}
 
+start_pre() {
+	if [ -n "$ipaddr $ipaddrs" ]
+	then
+		for i in $ipaddr $ipaddrs
+		do
+			if [ "${i##*/}" = "$i" ]
+			then
+				echo
+				ewarn "You probably want to add a netmask to the ipaddr $i defined in $settings."
+				ewarn "Example: ipaddr=\"192.168.0.1/24\""
+				echo
+			fi
+		done
+	fi
+}
+
 ezdns() {
 	# This function generates a resolv.conf entry, which ezresolv() passes to resolvconf
 	[ -n "$domain" ] && echo "domain $domain"
